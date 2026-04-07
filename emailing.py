97cba50f5433ff787fb87b0d947a39a58bf38467
AJ-18 <arjun.suresh1@marist.edu>
2026-04-06 20:40:19 -0400

    Emailing object detection with image attachment

--- emailing.py
+++ emailing.py
@@ -1,4 +1,31 @@
+import smtplib
+import imghdr
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PASSWORD = os.getenv("APP_PASS")
+SENDER = os.getenv("EMAIL_ADD")
+RECEIVER = os.getenv("EMAIL_ADD")
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Object noticed!"
+    email_message.set_content("Hey, just noticed an object appear!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
 
 
-def send_email():
-    print("Email was sent!")
+if __name__ == "__main__":
+    send_email(image_path="images/10.png")
